49b3052f27eae6bb71920dcc1ee67a1b9eecca60
Laura <l4depende@gmail.com>
2016-11-14 09:15:32 +0100

    Read and Write Files

---
+++ ReadAndWriteFiles/ReadAndWrite.R
@@ -0,0 +1,2 @@
+data <- read.table("/Volumes/USB DISK/MPObligatoria/Introducción a la ciencia de datos/Ejercicios II/array_datos.txt",sep = "\t")
+write.csv(data, "/Users/lala/DataScience/DataScienceInR/out.csv")
